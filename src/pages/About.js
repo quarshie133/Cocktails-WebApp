cd9b99ec7b5ee7691280ac83dc0bb63e1a632266
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-07-10 12:45:37 +0000

    completed development

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi animi quasi accusantium minima illum pariatur odio similique omnis. Illo rem, eum sed et voluptate eligendi labore voluptatem. Magnam, at ab aperiam ipsam veritatis hic saepe minima quo voluptatem, nobis vitae?</p>
+    </section>
+  )
+}
+
+export default About
